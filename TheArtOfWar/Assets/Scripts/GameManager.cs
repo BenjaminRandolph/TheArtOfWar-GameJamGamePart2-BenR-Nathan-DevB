15c850f5b9f1b9a74a1984bcb76c00a8e45175e3
drbryce27 <drbryce27@gmail.com>
2025-03-29 19:07:57 -0700

    Menu, Stage, Pause

--- TheArtOfWar/Assets/Scripts/GameManager.cs
+++ TheArtOfWar/Assets/Scripts/GameManager.cs
@@ -27,10 +27,16 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     GameObject pauseMenu;
 
+    [SerializeField]
+    GameObject stage;
+
+    bool mainMenuActive = true;
+
+    bool pauseMenuActive = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start(){
 
-        bool mainMenu = true;
         playButtonMain.SetActive(true);
         exitButtonMain.SetActive(true);
         settingsButtonMain.SetActive(true);
@@ -41,33 +47,53 @@ public class GameManager : MonoBehaviour
         settingsButton.SetActive(false);
         pauseMenu.SetActive(false);
 
+        stage.SetActive(false);
+
     }
 
     // Update is called once per frame
     void Update(){
 
         
-        if(mainMenu){
-            //if(playButtonMain.GetComponent<playButton>().clicked){}
+        if(mainMenuActive){
 
+            if(playButtonMain.GetComponent<playButtonMain>().clicked){
+
+                mainMenuActive = false;
+                playButtonMain.SetActive(false);
+                exitButtonMain.SetActive(false);
+                settingsButtonMain.SetActive(false);
+                mainMenu.SetActive(false);
+
+                stage.SetActive(true);
+
+            }
 
         }
         else{
-            if(playButton.GetComponent<playButton>().clicked){
 
-                playButton.SetActive(false);
-                exitButton.SetActive(false);
-                settingsButton.SetActive(false);
-                pauseMenu.SetActive(false);
+            // The pause menu is open, play and esc can close it
+            if(!pauseMenuActive && Input.GetKeyDown("escape")){
 
-            }
-            else if(Input.GetKeyDown("escape")){
-
-                playButton.GetComponent<playButton>().clicked = false;
                 playButton.SetActive(true);
                 exitButton.SetActive(true);
                 settingsButton.SetActive(true);
                 pauseMenu.SetActive(true);
+                pauseMenuActive = true;
+
+                Time.timeScale = 0;
+
+            }
+
+            // The pause menu is closed, esc can open it
+            else if(pauseMenuActive && (playButton.GetComponent<playButton>().clicked || Input.GetKeyDown("escape"))){
+                
+                playButton.SetActive(false);
+                exitButton.SetActive(false);
+                settingsButton.SetActive(false);
+                pauseMenu.SetActive(false);
+                pauseMenuActive = false;
+                playButton.GetComponent<playButton>().clicked = false;
 
             }
 
--- TheArtOfWar/Assets/Scripts/playButtonMain.cs
+++ TheArtOfWar/Assets/Scripts/playButtonMain.cs
@@ -2,15 +2,10 @@ using UnityEngine;
 
 public class playButtonMain : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public bool clicked = false;
+    public void closePauseMenu(){
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        clicked = true;
+
     }
 }
